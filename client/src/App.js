eae9f3636b42a2fca18c5b22eb1405898f4fb2db
vy3191 <venkyafhs@gmail.com>
2019-10-13 20:41:33 -0400

    Started fetching data from the server and stored in local storage

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {useLocalStorage} from './utils/Input';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useLocalStorage('data')
+
+   useEffect(() => {
+      axios.get(`http://localhost:5000/api/players`)
+           .then( response => {
+              console.log(response.data);
+              setUsers(response.data)
+           })
+           .catch(error => {
+              console.log(error);
+           })
+   },[]) 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Advanced React Sprint Challenge</h1>
+      
     </div>
   );
 }
---
+++ client/src/components/Users.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Users extends Component {
+  construction
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
