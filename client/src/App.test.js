a0d9ae701db84078fae6ae0695e6802f0543cd87
vy3191 <venkyafhs@gmail.com>
2019-10-14 20:55:10 -0400

    Added more tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,16 +2,17 @@ import React from 'react';
 import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import App from './App';	
-
+import Users from './components/Users';
 
 afterEach(rtl.cleanup);
 
 
+describe('It should render the App component without crushing', () => {
+    test('renders without crashing', async () => {	
+        const wrapper = await rtl.render(<App />)
+        const element = wrapper.getByText(/Advanced React Sprint Challenge/i)
+        expect(element).toBeDefined(); 
+        expect(element).toBeVisible();
+    })    
 
-
-
-it('renders without crashing', async () => {	
- const wrapper = await rtl.render(<App />)
-   const element = wrapper.getByText(/Advanced React Sprint Challenge/i)
-  expect(element).toBeVisible()
-});
+})
--- client/src/components/user.test.js
+++ client/src/components/user.test.js
@@ -6,10 +6,24 @@ import User from './User';
 
 afterEach(rtl.cleanup);
 
-test('Render the app', async () => {
-   const wrapper  = await rtl.render(<User />)
-   const name = wrapper.queryByText(/Name:/i);
-   expect(name).toBeDefined()
-  
-});
+describe('Testing user component', () => {
+      test('It should test and run h2 element for user name', async () => {
+         const wrapper  = await rtl.render(<User />)
+         const name = wrapper.queryByText(/Name:/i);
+         expect(name).toBeDefined() 
+         
+      })
 
+      test('It should test and run h2 element for user country', async () => {
+         const wrapper  = await rtl.render(<User />)
+         const country = wrapper.queryByText(/country:/i);
+         expect(country).toBeDefined()  
+      })
+
+      test('It should test and run h2 element for user country', async () => {
+         const wrapper  = await rtl.render(<User />)
+         const search = wrapper.queryByText(/searches:/i);
+         expect(search).toBeDefined()  
+      })
+
+})
